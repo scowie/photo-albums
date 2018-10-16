acac8508c0be958de33a4e133f57b6562abdb202
Stephen <steve.cowie@inmar.com>
2018-10-16 05:14:32 -0400

    added user sign-in with amplify cli using cognito

---
+++ build/precache-manifest.382819a16b1a8eb1a031f0d83a6a7ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "528c7d865202a94809df",
+    "url": "/static/js/main.528c7d86.chunk.js"
+  },
+  {
+    "revision": "43fb4cf4fec9facb2320",
+    "url": "/static/js/1.43fb4cf4.chunk.js"
+  },
+  {
+    "revision": "528c7d865202a94809df",
+    "url": "/static/css/main.4485616c.chunk.css"
+  },
+  {
+    "revision": "234415a88dd7be6339a5e41f24deaa11",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
 
 class App extends Component { 
   render() { 
@@ -11,4 +15,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
