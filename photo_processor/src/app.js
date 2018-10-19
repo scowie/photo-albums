0d8275dad181bc82b9f8eebf4e7eb339f996fadd
Stephen <steve.cowie@inmar.com>
2018-10-19 16:51:07 -0400

    update to add image data entry into dynamoDb on upload

--- photo_processor/src/app.js
+++ photo_processor/src/app.js
@@ -1,11 +1,17 @@
-// photo_processor/src/app.js
 const AWS = require('aws-sdk')
+const DynamoDBDocClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10'
+})
+const uuidv4 = require('uuid/v4')
 const S3 = new AWS.S3({ signatureVersion: 'v4' })
 // Note: Sharp requires native extensions. To get sharp to install from NPM in a
 // way that's compatible with the Amazon Linux environment that AWS runs Node.js
 // on, we can use this command: docker run -v "$PWD":/var/task lambci/lambda:build-nodejs8.10 npm install
 const Sharp = require('sharp')
 // We'll expect these environment variables to be defined when the Lambda function is deployed
+const DYNAMODB_PHOTOS_TABLE_NAME = process.env.DYNAMODB_PHOTOS_TABLE_ARN.split(
+  '/'
+)[1]
 const THUMBNAIL_WIDTH = parseInt(process.env.THUMBNAIL_WIDTH, 10)
 const THUMBNAIL_HEIGHT = parseInt(process.env.THUMBNAIL_HEIGHT, 10)
 function thumbnailKey (filename) {
@@ -56,11 +62,39 @@ async function resize (bucketName, key) {
     }
   }
 }
+function storePhotoInfo (item) {
+  const params = {
+    Item: item,
+    TableName: DYNAMODB_PHOTOS_TABLE_NAME
+  }
+  return DynamoDBDocClient.put(params).promise()
+}
+async function getMetadata (bucketName, key) {
+  const headResult = await S3.headObject({
+    Bucket: bucketName,
+    Key: key
+  }).promise()
+  return headResult.Metadata
+}
 async function processRecord (record) {
   const bucketName = record.s3.bucket.name
   const key = record.s3.object.key
+
   if (key.indexOf('uploads') !== 0) return
-  return resize(bucketName, key)
+
+  const metadata = await getMetadata(bucketName, key)
+  const sizes = await resize(bucketName, key)
+  const id = uuidv4()
+  const item = {
+    id: id,
+    owner: metadata.owner,
+    photoAlbumId: metadata.albumid,
+    bucket: bucketName,
+    thumbnail: sizes.thumbnail,
+    fullsize: sizes.fullsize,
+    createdAt: new Date().getTime()
+  }
+  await storePhotoInfo(item)
 }
 exports.lambda_handler = async (event, context, callback) => {
   try {
