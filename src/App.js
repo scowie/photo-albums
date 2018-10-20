dd62bff99a59ba8abf462bacdf47006c2d3fb55e
Stephen <steve.cowie@inmar.com>
2018-10-20 12:34:54 -0400

    added paging of photo results

--- src/App.js
+++ src/App.js
@@ -42,11 +42,12 @@ const ListAlbums = `query ListAlbums {
   }
 }`
 
-const GetAlbum = `query GetAlbum($id: ID!) {
+const GetAlbum = `query GetAlbum($id: ID!, $nextTokenForPhotos: String) {
   getAlbum(id: $id) {
     id
     name
-    photos {
+    photos(sortDirection: DESC, nextToken: $nextTokenForPhotos) {
+      nextToken
       items {
         thumbnail {
           width
@@ -54,11 +55,10 @@ const GetAlbum = `query GetAlbum($id: ID!) {
           key
         }
       }
-      nextToken
     }
   }
 }
-`
+`;
 
 const SubscribeToNewAlbums = `
   subscription OnCreateAlbum {
@@ -199,31 +199,58 @@ class NewAlbum extends Component {
 }
 
 class AlbumDetailsLoader extends React.Component {
-  render () {
-    return (
-      <Connect query={graphqlOperation(GetAlbum, { id: this.props.id })}>
-        {({ data, loading, errors }) => {
-          if (loading) {
-            return <div>Loading...</div>
-          }
-          if (errors.length > 0) {
-            return <div>{JSON.stringify(errors)}</div>
-          }
-          if (!data.getAlbum) return
-          return <AlbumDetails album={data.getAlbum} />
-        }}
-      </Connect>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextTokenForPhotos: null,
+      hasMorePhotos: true,
+      album: null,
+      loading: true
+    }
+  }
+  async loadMorePhotos() {
+    if (!this.state.hasMorePhotos) return;
+    this.setState({ loading: true });
+    const { data } = await API.graphql(graphqlOperation(GetAlbum, {id: this.props.id, nextTokenForPhotos: this.state.nextTokenForPhotos}));
+    let album;
+    if (this.state.album === null) {
+      album = data.getAlbum;
+    } else {
+      album = this.state.album;
+      album.photos.items = album.photos.items.concat(data.getAlbum.photos.items);
+    }
+    this.setState({ 
+      album: album,
+      loading: false,
+      nextTokenForPhotos: data.getAlbum.photos.nextToken,
+      hasMorePhotos: data.getAlbum.photos.nextToken !== null
+    });
+  }
+  componentDidMount() {
+    this.loadMorePhotos();
+  }
+  render() {
+    return <AlbumDetails loadingPhotos={this.state.loading} album={this.state.album} loadMorePhotos={this.loadMorePhotos.bind(this)} hasMorePhotos={this.state.hasMorePhotos}/>;
   }
 }
 
 class AlbumDetails extends Component {
   render() {
+    if (!this.props.album) return 'Loading album...';
     return (
       <Segment>
         <Header as='h3'>{this.props.album.name}</Header>
         <S3ImageUpload albumId={this.props.album.id}/>        
         <PhotosList photos={this.props.album.photos.items} />
+        {
+          this.props.hasMorePhotos && 
+          <Form.Button
+            onClick={this.props.loadMorePhotos}
+            icon='refresh'
+            disabled={this.props.loadingPhotos}
+            content={this.props.loadingPhotos ? 'Loading...' : 'Load more photos'}
+          />
+        }
       </Segment>
     )
   }
