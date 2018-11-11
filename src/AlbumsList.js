6a94a64c18ba503ed3d19228312a53bfe674ac3c
Stephen <steve.cowie@inmar.com>
2018-11-11 09:34:13 -0500

    fix programmatic routing using Redirect correctly

--- src/AlbumsList.js
+++ src/AlbumsList.js
@@ -1,64 +1,72 @@
-import React, { Component } from 'react'
-import { Header, List, Segment, Button, Icon } from 'semantic-ui-react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { List, Segment, Button, Icon } from "semantic-ui-react";
+import { Redirect, NavLink } from "react-router-dom";
 
+function makeComparator(key, order = "asc") {
+  return (a, b) => {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
 
-function makeComparator (key, order = 'asc') {
-    return (a, b) => {
-      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0
-  
-      const aVal = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key]
-      const bVal = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key]
-  
-      let comparison = 0
-      if (aVal > bVal) comparison = 1
-      if (aVal < bVal) comparison = -1
-  
-      return order === 'desc' ? comparison * -1 : comparison
-    }
+    const aVal = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const bVal = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
+
+    return order === "desc" ? comparison * -1 : comparison;
+  };
 }
 
 class AlbumsList extends Component {
-  albumItems () {
-    return this.props.albums.sort(makeComparator('name')).map(album => (
+  state = {
+    shouldNavigateToNewAlbumPage: false
+  };
+
+  navigateToNewAlbumPage = () => {
+    return this.setState({ shouldNavigateToNewAlbumPage: true });
+  };
+
+  albumItems() {
+    return this.props.albums.sort(makeComparator("name")).map(album => (
       <List.Item key={album.id}>
-      <Segment>
-        <NavLink to={`/albums/${album.id}`}>{album.name}</NavLink>
-      </Segment>
+        <Segment>
+          <NavLink to={`/albums/${album.id}`}>{album.name}</NavLink>
+        </Segment>
       </List.Item>
-    ))
+    ));
   }
 
-  render () {
-    return (
+  render() {
+    return this.state.shouldNavigateToNewAlbumPage ? (
+      <Redirect to="/newalbum" />
+    ) : (
       <div>
         <div style={styles.titleBarContainer}>
-        <div style={styles.titleBarTitleContainer}><h2>Galleries</h2>
-            
-        </div>
-        <Button icon>
-            Create New Gallery&nbsp;&nbsp;<Icon name="plus"></Icon>
-        </Button>
+          <div style={styles.titleBarTitleContainer}>
+            <h2>Galleries</h2>
+          </div>
+          <Button icon onClick={this.navigateToNewAlbumPage}>
+            Create New Gallery&nbsp;&nbsp;
+            <Icon name="plus" />
+          </Button>
         </div>
         <List divided relaxed>
           {this.albumItems()}
         </List>
-        <NavLink to={'/newalbum'}>Add New Album</NavLink>
       </div>
-    )
+    );
   }
 }
 
 const styles = {
-    titleBarContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    titleBarTitleContainer: {
-        display: 'flex',
-        alignItems: 'center'
-    }
-
-}
-export default AlbumsList
+  titleBarContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  titleBarTitleContainer: {
+    display: "flex",
+    alignItems: "center"
+  }
+};
+export default AlbumsList;
