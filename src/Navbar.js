3c904195d47cef0291744b9af0334049ab038079
Stephen <steve.cowie@inmar.com>
2018-11-15 05:23:58 -0500

    add menu dropdown and icon placeholder to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,41 +1,68 @@
-import React, { Component } from 'react'
-import { Menu, Dropdown } from 'semantic-ui-react'
-import { Auth } from 'aws-amplify'
+import React, { Component } from "react";
+import { Menu, Dropdown, Icon } from "semantic-ui-react";
+import { Auth } from "aws-amplify";
 
 class Navbar extends Component {
+  state = {};
 
-    state = {}
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  getAuthenticatedUser = async () => await Auth.currentAuthenticatedUser();
 
-    getAuthenticatedUser = async () => await Auth.currentAuthenticatedUser()
+  signOutUser = () =>
+    Auth.signOut().then(() => {
+      window.location.reload();
+    });
 
-    signOutUser = () => Auth.signOut().then(() => {
-        window.location.reload()
-    })
+  getUsername = () =>
+    this.state.user && this.state.user.username ? this.state.user.username : "";
 
-    getUsername = () => this.state.user && this.state.user.username ? this.state.user.username : ""
+  componentDidMount() {
+    this.getAuthenticatedUser().then(user => this.setState({ user: user }));
+  }
 
-    componentDidMount() {
-        this.getAuthenticatedUser().then(user => this.setState({user: user}))
-    }
+  // todo:  make array of menu item names
+  // set active item and show active item in navbar
 
-    render () {
-        const { activeItem } = this.state
-        return (
-            <Menu id="navbar">
-                <Menu.Menu position='right'>
-                    <Menu.Item>
-                    <Dropdown text={this.getUsername()} pointing className="link item">
-                        <Dropdown.Menu>
-                            <Dropdown.Item onClick={this.signOutUser}>Logout</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    </Menu.Item>
-                </Menu.Menu>
-            </Menu>
-        )
-    }
+  render() {
+    const { activeItem } = this.state;
+    const menuTrigger = (
+      <span>
+        <Icon name={"th"} /> MENU
+      </span>
+    );
+    return (
+      <Menu id="navbar">
+        <Menu.Item>-------------------LOGO--------------------</Menu.Item>
+
+        <Menu.Item id={"mega-menu"}>
+          <Dropdown
+            icon={null}
+            trigger={menuTrigger}
+            pointing
+            className="link item"
+          >
+            <Dropdown.Menu>
+              <Dropdown.Item>Menu Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
+
+        <Menu.Item position="right">
+          <Dropdown
+            text={this.getUsername()}
+            icon={null}
+            pointing
+            className="link item"
+          >
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={this.signOutUser}>Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
