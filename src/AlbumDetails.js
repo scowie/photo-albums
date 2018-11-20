bbba0f8859cfa884fcc96844ea5045c99bc55faa
Stephen <steve.cowie@inmar.com>
2018-11-20 07:19:03 -0500

    wip to use actions dropdown on album details component

--- src/AlbumDetails.js
+++ src/AlbumDetails.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import PhotosList from "./PhotosList";
 import S3ImageUpload from "./S3ImageUpload";
-import { Form, Button, Icon, Divider } from "semantic-ui-react";
+import { Form, Button, Icon, Divider, Dropdown } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import { API, graphqlOperation } from "aws-amplify";
 
 class AlbumDetails extends Component {
-    state = {
-        saveInProgress: false,
-        hasUnsavedChanges: false
-    }
+  state = {
+    saveInProgress: false,
+    hasUnsavedChanges: false
+  };
 
-    saveAlbumChanges = async () => {
-        const UpdateAlbum = `mutation UpdateAlbum($id: ID!, $name: String, $sortPosition: Int, $isVisible: Boolean) {
+  saveAlbumChanges = async () => {
+    const UpdateAlbum = `mutation UpdateAlbum($id: ID!, $name: String, $sortPosition: Int, $isVisible: Boolean) {
             updateAlbum(input: {id: $id, name: $name, sortPosition: $sortPosition, isVisible: $isVisible}) {
               id
               name
@@ -21,27 +21,27 @@ class AlbumDetails extends Component {
             }
           }`;
 
-        this.setState({saveInProgress: true}, async () => {
-            const result = await API.graphql(
-                graphqlOperation(UpdateAlbum, {
-                  id: this.state.album.id,
-                  name: this.state.album.name,
-                  sortPosition: this.state.album.sortPosition,
-                  isVisible: this.state.album.isVisible
-                })
-              );
-              this.setState({saveInProgress: false})
-              return result;
+    this.setState({ saveInProgress: true }, async () => {
+      const result = await API.graphql(
+        graphqlOperation(UpdateAlbum, {
+          id: this.state.album.id,
+          name: this.state.album.name,
+          sortPosition: this.state.album.sortPosition,
+          isVisible: this.state.album.isVisible
         })
-      };
+      );
+      this.setState({ saveInProgress: false });
+      return result;
+    });
+  };
 
-      componentDidUpdate(prevProps, prevState) {
-        if (prevProps.album !== this.props.album) {
-          this.setState({ album: this.props.album });
-        } else if (prevState.album && prevState.album !== this.state.album) {
-          this.setState({ hasUnsavedChanges: true });
-        }
-      }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.album !== this.props.album) {
+      this.setState({ album: this.props.album });
+    } else if (prevState.album && prevState.album !== this.state.album) {
+      this.setState({ hasUnsavedChanges: true });
+    }
+  }
 
   render() {
     if (!this.props.album) return "Loading album...";
@@ -63,6 +63,19 @@ class AlbumDetails extends Component {
               </Button>
             </NavLink>
             <S3ImageUpload albumId={this.props.album.id} />
+            <Dropdown
+              text="Actions&nbsp;&nbsp;&nbsp;"
+              icon="content"
+              floating
+              button
+              className="icon"
+            >
+              <Dropdown.Menu>
+                <Dropdown.Item icon="attention" text="Important" />
+                <Dropdown.Item icon="comment" text="Announcement" />
+                <Dropdown.Item icon="conversation" text="Discussion" />
+              </Dropdown.Menu>
+            </Dropdown>
             <Button
               size="medium"
               style={{ marginLeft: "10px" }}
